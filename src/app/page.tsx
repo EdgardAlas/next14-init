345edbc6ffbfd9b7028965130966745915165486
Edgard Alas <edgardalasu@gmail.com>
2024-05-08 19:42:08 -0600

    feat: example main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@components/ui/button';
+
 export default function Home() {
 	return (
-		<>
-			<h1>Next14 Template</h1>
-		</>
+		<main>
+			<header className='flex min-h-96 flex-col items-center justify-center gap-4 bg-gray-100 py-2 sm:py-12'>
+				<h1 className='text-2xl font-bold sm:text-4xl'>Next.js 14 template</h1>
+				<Button>This is a button from shadcn</Button>
+			</header>
+		</main>
 	);
 }
