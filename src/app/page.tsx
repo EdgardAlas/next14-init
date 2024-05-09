1417e5d730bf59ad63c934b3e7c70d4c230e3068
Edgard Alas <edgardalasu@gmail.com>
2024-05-08 19:26:50 -0600

    feat: formatting rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
-  return (
-    <>
-      <h1>Next14 Template</h1>
-    </>
-  );
+	return (
+		<>
+			<h1>Next14 Template</h1>
+		</>
+	);
 }
